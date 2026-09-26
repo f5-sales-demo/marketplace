86c20c58ae3a9a02f5ecdbe5ab908a029afc925d
Robin Mordasiewicz <robin@mordasiewicz.com>
2026-09-25 22:39:15 -0400

    fix(xorg): converge virtual camera setup (#1413)
    Co-authored-by: Robin Mordasiewicz <r.mordasiewicz@f5.com>

--- plugins/xorg/scripts/xorgctl_lib/common.py
+++ plugins/xorg/scripts/xorgctl_lib/common.py
@@ -10,7 +10,7 @@ import socket
 import struct
 import subprocess
 
-VERSION = "1.1.5"
+VERSION = "1.1.6"
 ROOT = pathlib.Path(
     os.environ.get("XORGCTL_STATE", pathlib.Path.home() / ".local/state/xorgctl")
 )
